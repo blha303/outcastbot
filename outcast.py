4e6c9494005dff1ea0217fc46e116d5c9ec6fe51
Alyssa Smith <suv@suv.id.au>
2020-05-01 23:23:50 +0800

    Removed emoji functionality

--- outcast.py
+++ outcast.py
@@ -1,66 +1,11 @@
 #!/usr/bin/env python3
 import discord
 from io import BytesIO
-import aiohttp
 from subprocess import check_output
 
 client = discord.Client()
 
 __cw_help = "Usage: !cw <message id or link>:<trigger warning message>, ... - e.g !cw 12345:trigger 2, 67890:trigger 3. Link required if cwing message in another channel (including \"server/channel/message\" IDs)"
-__emoji_help = """Usage:
-!emoji import <custom emoji to import> <another custom emoji> ... - emoji will be imported using the name from the other server
-!emoji image <name> - attach an image and specify the name to import. image must be smaller than 256kb"""
-
-def get_emoji_count(guild):
-    emojis = guild.emojis
-    normal = [e for e in emojis if not e.animated]
-    animated = [e for e in emojis if e.animated]
-    if not guild.premium_tier:
-        return "{0}/50 normal, {1}/50 animated".format(len(normal), len(animated))
-    elif guild.premium_tier in [1,2]:
-        return "{0}/{2} normal, {1}/{2} animated".format(len(normal), len(animated), 50+(guild.premium_tier*50))
-    else:
-        return "{0}/250 normal, {1}/250 animated".format(len(normal), len(animated))
-
-async def emoji(message):
-    content = message.content[6:].strip()
-    try:
-        cmd, *args = content.split()
-        if cmd not in ["import", "image"]:
-            content = None
-    except ValueError:
-        content = None
-    if not content:
-        try:
-            await message.author.send(__emoji_help)
-        except discord.Forbidden:
-            await message.channel.send(__emoji_help)
-        return
-    if cmd == "import":
-        out = []
-        for emoji_inp in args:
-            animated,shortname,emoji_id = emoji_inp[1:-1].split(":")
-            async with aiohttp.ClientSession() as session:
-                async with session.get(f"https://cdn.discordapp.com/emojis/{emoji_id}." + ("gif" if animated else "png")) as resp:
-                    img_bytes = await resp.read()
-            try:
-                emoji = await message.guild.create_custom_emoji(name=shortname, image=img_bytes, reason=f"Added by {message.author.name}#{message.author.discriminator}")
-            except discord.errors.HTTPException as e:
-                await message.channel.send("!emoji failed on {}: {}".format(emoji_inp[1:-1], e.text.split("\n")[1]))
-                continue
-            out.append(str(emoji))
-        await message.channel.send(get_emoji_count(message.guild) + "\n" + " ".join(out))
-    elif cmd == "image":
-        out = []
-        for arg,attachment in zip(args, message.attachments):
-            img_bytes = await attachment.read()
-            try:
-                emoji = await message.guild.create_custom_emoji(name=arg, image=img_bytes, reason=f"Added by {message.author.name}#{message.author.discriminator}")
-            except discord.errors.HTTPException as e:
-                await message.channel.send("!emoji failed on {}: {}".format(arg, e.text.split("\n")[1]))
-                continue
-            out.append(str(emoji))
-        await message.channel.send(get_emoji_count(message.guild) + "\n" + " ".join(out))
 
 async def cw(message, *args):
     if args == ([''],):
@@ -121,12 +66,6 @@ async def on_message(message):
     if message.content.lower().startswith("!boop") and message.author.permissions_in(message.channel).administrator:
         await message.channel.send("I've been booped! Back shortly...")
         await client.logout()
-
-    if message.content.lower().startswith("!emoji"):
-        if message.author.permissions_in(message.channel).manage_emojis:
-            await emoji(message)
-        else:
-            await message.channel.send("No 'Manage Emoji' permission")
 
     if message.content.lower().startswith("!cw"):
         if message.author.permissions_in(message.channel).manage_messages:
